cb7c8feab618b90483dfed909fa4de8d36d10049
mosesmbugua <swahili1963@gmail.com>
2022-10-01 02:12:03 -0700

    New Appointment list Subcomponent

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,26 @@
 <template>
-  <div id="main-app" class="container">
-    <h4>{{ title }}</h4>
-    <font-awesome-icon icon="plus" class="mr-2" />Add Appointment
-  </div>
+  <AppointmentList :appointments="appointments" />
 </template>
 
 <script lang="ts">
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import axios from "axios";
+import AppointmentList from "./components/AppointmentList.vue";
 export default {
   name: "MainApp",
-  data() {
+  data: function () {
     return {
       title: "Appointment List",
+      appointments: [],
     };
   },
   components: {
-    FontAwesomeIcon,
+    AppointmentList,
+  },
+  mounted() {
+    axios
+      .get("./data/appointments.json")
+      .then((response) => (this.appointments = response.data))
+      .catch((error) => console.log(error));
   },
 };
 </script>
---
+++ src/components/AppointmentList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="main-app" class="container">
+    <h4>{{ title }}</h4>
+    <font-awesome-icon icon="plus" class="mr-2" />Add Appointment
+    <div v-for="(appointment, index) in appointments" :key="index">
+      <h4>{{ appointment.petName }}</h4>
+      <p>{{ appointment.aptNotes }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "AppointmentList",
+  props: ["appointments"],
+  components: [FontAwesomeIcon],
+  data() {},
+};
+</script>
+
+<style scope=""></style>
