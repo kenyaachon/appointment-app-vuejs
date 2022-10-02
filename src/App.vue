dfa122bcc58994d10f80bf4bdfb1506525db6e36
mosesmbugua <swahili1963@gmail.com>
2022-10-01 21:35:04 -0700

    Add functionality for deleting items

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <AppointmentList :appointments="appointments" />
+  <h4>{{ title }}</h4>
+  <div id="main-app" class="container">
+    <button class="btn btn-sm btn-primary">
+      <FontAwesomeIcon icon="plus" class="mr-2" /></button
+    >Add Appointment
+
+    <div class="row justify-content-center">
+      <AppointmentList :appointments="appointments" @delete="deleteItem" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import axios from "axios";
 import AppointmentList from "./components/AppointmentList.vue";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import _ from "lodash";
 export default {
   name: "MainApp",
   data: function () {
@@ -15,12 +26,24 @@ export default {
   },
   components: {
     AppointmentList,
+    FontAwesomeIcon,
   },
   mounted() {
     axios
       .get("./data/appointments.json")
       .then((response) => (this.appointments = response.data))
       .catch((error) => console.log(error));
+  },
+  methods: {
+    deleteItem: function (appointment: any) {
+      // this.appointments = this.appointments.filter((item) => {
+      //   if (item.petName == appointment.petName) {
+      //     return false;
+      //   }
+      //   return true;
+      // });
+      this.appointments = _.without(this.appointments, appointment);
+    },
   },
 };
 </script>
--- src/components/AppointmentList.vue
+++ src/components/AppointmentList.vue
@@ -1,22 +1,51 @@
 <template>
-  <div id="main-app" class="container">
-    <h4>{{ title }}</h4>
-    <font-awesome-icon icon="plus" class="mr-2" />Add Appointment
-    <div v-for="(appointment, index) in appointments" :key="index">
-      <h4>{{ appointment.petName }}</h4>
-      <p>{{ appointment.aptNotes }}</p>
+  <div class="col-12 col-md-10 col-lg-7">
+    <div class="list-group list-group-flush">
+      <div
+        class="list-group-item"
+        v-for="(appointment, index) in appointments"
+        :key="index"
+      >
+        <button
+          class="mr-2 btn btn-sm btn-danger"
+          @click="deleteItem(appointment)"
+        >
+          <FontAwesomeIcon icon="trash" />
+        </button>
+        <div class="w-100">
+          <div class="d-flex justify-content-between">
+            <span class="h4 text-primary">{{ appointment.petName }}</span>
+            <span class="float-right">{{
+              formattedDate(appointment.aptDate)
+            }}</span>
+          </div>
+          <div class="owner-name">
+            <span class="font-weight-bold text-primary mr-1">Owner:</span>
+            <span>{{ appointment.petOwner }}</span>
+          </div>
+          <div>{{ appointment.aptNotes }}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
+import moment from "moment";
 export default {
   name: "AppointmentList",
   props: ["appointments"],
-  components: [FontAwesomeIcon],
   data() {},
+  components: { FontAwesomeIcon },
+  methods: {
+    formattedDate: function (date: any) {
+      return moment(new Date(date)).format("MM-DD-YY, h:mm a");
+    },
+    deleteItem(appointment: any) {
+      this.$emit("delete", appointment);
+    },
+  },
 };
 </script>
 
